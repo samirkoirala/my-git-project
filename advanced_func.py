eaf2ced85f05f28abac23f8a2611bf593a2a674f
samirkoirala <samirkoirala13@gmail.com>
2024-12-04 15:27:44 +0545

    2

--- advanced_func.py
+++ advanced_func.py
@@ -1,2 +1,17 @@
-def advanced_func();
+def advanced_func():
     return "Advanced Functionality"
+
+def advanced_func2():
+    return "Advanced Functionality 2"
+
+def advanced_func3():
+    return "Advanced Functionality 3"
+
+def advanced_func4():
+    return "Advanced Functionality 4"
+def advanced_func5():
+    return "Advanced Functionality 5"
+def advanced_func6():
+    return "Advanced Functionality 6"
+def advanced_func7():
+    return "Advanced Functionality 7"
